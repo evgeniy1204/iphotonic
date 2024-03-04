49050c95e67c2087277f92b27f91087db3d0ac23
ekuzmich <evgeny.kuzmich@av.by>
2024-03-04 14:33:35 +0300

    setting iphotonic.local domain

--- support/docker/containers/nginx/docker-entrypoint.sh
+++ support/docker/containers/nginx/docker-entrypoint.sh
@@ -6,10 +6,9 @@ mkdir -p /var/www/app/public
 find /etc/nginx/conf.d/ -name 'default.conf' -exec rm -f {} \;
 mkdir -p /etc/nginx/sites-enabled
 
-ln -s /etc/nginx/sites-available/iphotonic.dev.conf /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/api.conf /etc/nginx/sites-enabled/
 cp /etc/nginx/sites-available/default_81.conf /etc/nginx/sites-enabled/default_81.conf
 
 find /etc/nginx/sites-enabled -type f -exec sed -i "s/<<<DOMAIN>>>/${DOMAIN}/g" {} +
-find /etc/nginx/sites-available -type f -exec sed -i "s/<<<DOMAIN>>>/${DOMAIN}/g" {} +
 
 exec "nginx"
