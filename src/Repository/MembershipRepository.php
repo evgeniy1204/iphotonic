b354f2ee3b88479a1f482e42c19aa18f61fd11e5
nikita <moviex@inbox.ru>
2024-03-09 15:12:07 +0300

    deleted comments in repo

--- src/Repository/MembershipRepository.php
+++ src/Repository/MembershipRepository.php
@@ -20,29 +20,4 @@ class MembershipRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Membership::class);
     }
-
-    //    /**
-    //     * @return Membership[] Returns an array of Membership objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Membership
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
