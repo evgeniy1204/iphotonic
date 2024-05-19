d40064afbdbdd3f600d7c5b4f2eebfdae28ec414
ekuzmich <evgeny.kuzmich@av.by>
2024-05-19 18:02:42 +0300

    fix tiny img urls

--- public/app/scripts/tiny_mce_app.js
+++ public/app/scripts/tiny_mce_app.js
@@ -4,5 +4,6 @@ tinymce.init({
     plugins: 'anchor autolink code fullscreen charmap preview codesample image link lists media searchreplace table visualblocks wordcount linkchecker',
     toolbar: 'code fullscreen | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | checklist numlist bullist indent outdent',
     images_upload_url: '/upload-files',
+    relative_urls: false,
     ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant")),
 });
