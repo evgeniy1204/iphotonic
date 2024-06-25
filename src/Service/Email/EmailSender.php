8fceb148367ac99294cafa1588fbc3d4c87a4b22
ekuzmich <evgeny.kuzmich@av.by>
2024-06-25 20:46:14 +0300

    filter spam

--- src/Service/Email/EmailSender.php
+++ src/Service/Email/EmailSender.php
@@ -31,7 +31,7 @@ readonly class EmailSender
 		$email = (new Email())
 			->from($this->emailSender)
 			->to($this->settingsProvider->getEmail() ?? '')
-			->to('kuzmich.zhek@gmail.com')
+			->addTo('kuzmich.zhek@gmail.com')
 			->subject($email->getSubject())
 			->html($email->getText());
 
